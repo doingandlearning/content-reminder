1792b905ba5c610f102be14249dcca6ac3ec5ad0
Kevin Cunningham <kevin@kevincunningham.co.uk>
2021-04-12 08:31:40 +0100

    Only fetch from WP for now.

--- sources-rss.js
+++ sources-rss.js
@@ -1,20 +1,20 @@
 const rss = [
   {
-    title: 'Kevin Cunningham',
-    url: 'https://kevincunningham.co.uk/feed.xml'
-  },
-  {
-    title: 'freeCodeCamp News',
-    url: 'https://www.freecodecamp.org/news/author/kevin-cunningham/rss/'
-  },
-  {
-    title: 'YouTube',
-    url: 'https://www.youtube.com/channel/UCtzNXx0YjJFvAuAPL9ZjQOw'
-  },
-  {
-    title: 'egghead',
-    url: 'https://app.egghead.io/instructors/kevin-cunningham/feed?user_email=kevin%40kevincunningham.co.uk&user_token=e8d87dbf-ecb3-48ad-8455-d46e618f638f'
+    title: "Kevin Cunningham",
+    url: "https://kevincunningham.co.uk/feed.xml"
   }
+  // {
+  // title: "freeCodeCamp News",
+  // url: "https://www.freecodecamp.org/news/author/kevin-cunningham/rss/"
+  // }
+  // {
+  // title: "YouTube",
+  // url: "https://www.youtube.com/channel/UCtzNXx0YjJFvAuAPL9ZjQOw"
+  // }
+  // {
+  // title: 'egghead',
+  // url: 'https://app.egghead.io/instructors/kevin-cunningham/feed?user_email=kevin%40kevincunningham.co.uk&user_token=e8d87dbf-ecb3-48ad-8455-d46e618f638f'
+  // }
 ];
 
 module.exports = rss;
